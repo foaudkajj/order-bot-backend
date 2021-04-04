56ce66bbbe5aeadaf058dd85108e256b7feb4f85
Muhammed Fuat Nuroğlu <fuat.nuroglu@dgpaysit.com>
2021-04-04 16:06:41 +0300

    mysql2 + sharedmodule

---
+++ src/shared.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { DevextremeLoadOptionsService } from './DB/Helpers/devextreme-loadoptions';
+
+
+@Module({
+    imports: [],
+    controllers: [],
+    providers: [DevextremeLoadOptionsService],
+    exports: [DevextremeLoadOptionsService]
+})
+export class SharedModule { }
