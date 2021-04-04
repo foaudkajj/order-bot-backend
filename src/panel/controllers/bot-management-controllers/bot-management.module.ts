74fa0ec7cd60b85addd9ab64da41e7aed2ec9e24
Muhammed Fuat Nuroğlu <fuat.nuroglu@dgpaysit.com>
2021-04-04 16:13:03 +0300

    Category Apisi

---
+++ src/panel/controllers/bot-management-controllers/bot-management.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { CategoryModule } from './category-controller/category.module';
+import { OrderModule } from './order-controller/order.module';
+import { ProductModule } from './product-controller/product.module';
+
+@Module({
+    imports: [CategoryModule, ProductModule, OrderModule],
+    controllers: [],
+    providers: [],
+})
+export class BotManagementModule { }
--- src/panel/panel.module.ts
+++ src/panel/panel.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { BotManagementModule } from './controllers/bot-management-controllers/bot-management.module';
 import { RoleModule } from './controllers/role-controller/role.module';
 import { LoginModule } from './controllers/userController/user.module';
 
 @Module({
-    imports: [LoginModule, RoleModule],
+    imports: [LoginModule, RoleModule, BotManagementModule],
     controllers: [],
     providers: [],
 })
