93decdae6e94c4867b94f610f8f4eb4bb6772c95
Muhammed Fuat Nuroğlu <fuat.nuroglu@dgpaysit.com>
2021-04-04 16:11:15 +0300

    Kategoriler dinamik geliyor artik

--- src/bot/helpers/start-ordering-CB-handler.ts
+++ src/bot/helpers/start-ordering-CB-handler.ts
@@ -1,17 +1,18 @@
 import { OrderStatus } from "src/DB/enums/OrderStatus";
-import { getCustomRepository } from "typeorm";
-import { UserRepository } from "../custom-repositories/UserRepository";
+import { Category } from "src/DB/models/Category";
+import { InlineKeyboardButton } from "telegraf/typings/telegram-types";
+import { getCustomRepository, getRepository, Repository } from "typeorm";
+import { CustomerRepository } from "../custom-repositories/CustomerRepository";
 import { BotContext } from "../interfaces/BotContext";
 import { CallBackQueryResult } from "../models/CallBackQueryResult";
 import { OrdersInBasketCb } from "./get-orders-in-basket-CB-handler";
 
 export abstract class StartOrderingCb {
-
   public static async StartOrdering(ctx: BotContext) {
-    const userRepository = getCustomRepository(UserRepository);
+    const userRepository = getCustomRepository(CustomerRepository);
     let user = await userRepository.getUser(ctx);
     user.SelectedProducts = null;
-    await userRepository.update({ Id: user.Id }, user);
+    await userRepository.update({ TelegramId: user.TelegramId }, user);
     let orderDetails = await OrdersInBasketCb.GetOrdersInBasketByStatus(ctx, OrderStatus.InBasket);
     await this.ShowProductCategories(ctx, orderDetails);
 
@@ -21,18 +22,20 @@ export abstract class StartOrderingCb {
   static async ShowProductCategories(ctx: BotContext, orderDetails: string) {
     try {
       const orders = orderDetails === null ? 'Lütfen bir ürün seçiniz' : orderDetails;
+      var categories: Category[] = await getRepository(Category)
+        .find();
       await ctx.editMessageText(orders,
         {
           parse_mode: "HTML",
           reply_markup: {
             // one_time_keyboard: true,
             inline_keyboard:
-              [
-                [{ text: "🍖 Kebap 🍢", switch_inline_query_current_chat: 'Kebap' }],
-                [{ text: "🥤 İçecek 🍸", switch_inline_query_current_chat: "\u0130\u00E7ecek" }],
-                [{ text: "🍲 Çorba 🥣", switch_inline_query_current_chat: "\u00C7orba" }],
-                [{ text: "🍬 Tatlı 🍬", switch_inline_query_current_chat: "Tatl\u0131" }],
-                [{ text: "◀️ Ana Menüye Dön ◀️", callback_data: CallBackQueryResult.MainMenu }]
+              [...categories.map(mp => <InlineKeyboardButton[]>
+                (
+                  [{ text: mp.Name, switch_inline_query_current_chat: mp.CategoryKey }]
+                )
+              ),
+              [{ text: "◀️ Ana Menüye Dön ◀️", callback_data: CallBackQueryResult.MainMenu }]
               ]
           }
         });
