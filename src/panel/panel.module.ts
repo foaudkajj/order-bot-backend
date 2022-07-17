aff53b1a0f852a30c94c34c2a631ca4f60726026
mfuatnuroglu <mfuatnuroglu@gmail.com>
2022-07-17 16:07:53 +0300

    minor

--- src/panel/panel.module.ts
+++ src/panel/panel.module.ts
@@ -1,12 +1,12 @@
-import { Module } from '@nestjs/common';
-import { BotManagementModule } from './controllers/bot-management-controllers/bot-management.module';
-import { EntegrationsModule } from './controllers/entegrations-management/entegrations-management.module';
-import { RoleModule } from './controllers/role-controller/role.module';
-import { LoginModule } from './controllers/userController/user.module';
+import {Module} from '@nestjs/common';
+import {BotManagementModule} from './controllers/bot-management-controllers/bot-management.module';
+import {EntegrationsModule} from './controllers/entegrations-management/entegrations-management.module';
+import {RoleModule} from './controllers/role-controller/role.module';
+import {LoginModule} from './controllers/user-controller/user.module';
 
 @Module({
   imports: [LoginModule, RoleModule, BotManagementModule, EntegrationsModule],
   controllers: [],
-  providers: []
+  providers: [],
 })
 export class PanelModule {}
