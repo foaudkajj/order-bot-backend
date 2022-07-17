51380fa96e9b9e6a33c93368cb836db4a3b83fd3
mfuatnuroglu <mfuatnuroglu@gmail.com>
2022-07-17 14:27:07 +0300

    revert-git-config-core.ignorecase-false

--- src/bot/interfaces/session.ts
+++
@@ -1,7 +0,0 @@
-import {Scenes} from 'telegraf';
-import {SceneSession} from './scene-session';
-
-export interface Session extends Scenes.SceneSession<SceneSession> {
-  // will be available under `ctx.session.mySessionProp`
-  mySessionProp: number;
-}
