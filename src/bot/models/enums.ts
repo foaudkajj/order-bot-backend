3cc5662f76fe67b70d34e99a973eb6888f7d96b4
mfuatnuroglu <mfuatnuroglu@gmail.com>
2022-08-08 10:06:20 +0300

    comments

--- src/bot/models/enums.ts
+++ src/bot/models/enums.ts
@@ -1,13 +1,8 @@
 export enum CallBackQueryResult {
   StartOrdering = 'START_ORDERING',
   AddToBasketAndContinueShopping = 'ADD_TO_BASKET',
-  // AddToBasketAndCompleteOrder = "ADD_TO_BASKET_COMPLETE_ORDER",
   TrackOrder = 'ORDER_TRACKING',
   MyBasket = 'MY_ORDERS',
-  // AddressRequest = "ADRESS_REQUEST",
-  // SearchAgain = "SEARCH_AGAIN",
-  // IncreaseOrder = "IncreaseOrder",
-  // GoToPayment = "GO_TO_PAYMENT",
   CompleteOrder = 'COMPLETE_ORDER',
   AddProductAndCompleteOrder = 'ADD_PRODUCT_AND_COMPLETE_ORDER',
   EnterAddress = 'ENTER_ADDRESS',
