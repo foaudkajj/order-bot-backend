1e14dfa971d014a5da2e68f8f5fd4d77b72dc8f8
mfuatnuroglu <mfuatnuroglu@gmail.com>
2022-08-20 21:58:37 +0300

    refactoring

--- src/panel/controllers/role-controller/role.service.ts
+++ src/panel/controllers/role-controller/role.service.ts
@@ -7,13 +7,14 @@ import {DataSourceLoadOptionsBase} from 'src/panel/dtos/devextreme-query';
 import {GetRolesDto} from 'src/panel/dtos/get-roles-dto';
 import {RoleIdAndPermissions} from 'src/panel/dtos/role-id-and-permissions';
 import {UIResponseBase} from 'src/panel/dtos/ui-response-base';
-import {getManager} from 'typeorm';
+import {DataSource} from 'typeorm';
 
 @Injectable()
 export class RoleService {
   constructor(
     private permissionRepository: PermissionRepository,
     private roleRepository: RoleRepository,
+    private dataSource: DataSource,
   ) {}
 
   async GetRoles(query: DataSourceLoadOptionsBase) {
@@ -65,24 +66,36 @@ export class RoleService {
   }
 
   async SaveRolePermissions(roleIdAndPermissions: RoleIdAndPermissions) {
+    const queryRunner = this.dataSource.createQueryRunner();
+
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
     try {
-      await getManager().transaction(async transactionalEntityManager => {
-        await transactionalEntityManager.delete(RoleAndPermission, {
-          roleId: roleIdAndPermissions.roleId,
-        });
-        await transactionalEntityManager.insert(
-          RoleAndPermission,
-          roleIdAndPermissions.rolePermissions.map(
-            mp =>
-              <RoleAndPermission>{
-                roleId: roleIdAndPermissions.roleId,
-                permissionId: mp,
-              },
-          ),
-        );
+      await queryRunner.manager.delete(RoleAndPermission, {
+        roleId: roleIdAndPermissions.roleId,
       });
-    } catch (error) {
-      throw new HttpException(error, HttpStatus.INTERNAL_SERVER_ERROR);
+
+      await queryRunner.manager.insert(
+        RoleAndPermission,
+        roleIdAndPermissions.rolePermissions.map(
+          mp =>
+            <RoleAndPermission>{
+              roleId: roleIdAndPermissions.roleId,
+              permissionId: mp,
+            },
+        ),
+      );
+
+      await queryRunner.commitTransaction();
+    } catch (err) {
+      // since we have errors lets rollback the changes we made
+      await queryRunner.rollbackTransaction();
+
+      throw new HttpException(err, HttpStatus.INTERNAL_SERVER_ERROR);
+    } finally {
+      // you need to release a queryRunner which was manually instantiated
+      await queryRunner.release();
     }
   }
 
