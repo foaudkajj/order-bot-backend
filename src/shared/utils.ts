48169af78f6c66d89aa4d6bab814ad363fd2d712
mfuatnuroglu <mfuatnuroglu@gmail.com>
2024-05-01 13:28:06 +0300

    some refactoring

--- src/shared/utils.ts
+++ src/shared/utils.ts
@@ -1,3 +1,16 @@
 export const removeHtmlTags = (input: string) => {
   return input?.replace(/<[^>]*>/g, '');
 };
+
+export const safeJsonParse = <T = any>(input: string): T => {
+  if (!input) {
+    return undefined;
+  }
+
+  try {
+    return JSON.parse(input) as T;
+  } catch (err) {
+    console.error(err);
+    return undefined;
+  }
+};
