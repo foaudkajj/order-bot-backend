44e6427fecc065b5e9568dc6f269c8700f919be9
mfuatnuroglu <mfuatnuroglu@gmail.com>
2024-05-10 09:37:40 +0300

    Init packag version when app starts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,23 +1,26 @@
-import {Controller, Get} from '@nestjs/common';
+import {Controller, Get, OnModuleInit} from '@nestjs/common';
 import {AppService} from './app.service';
 import {AllowAnonymous} from './panel/decorators/public.decorator';
 import {UIResponseBase} from './panel/dtos';
 import {promises as fs} from 'fs';
 
 @Controller('api/app')
-export class AppController {
+export class AppController implements OnModuleInit {
   packageVersion: string;
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  @AllowAnonymous()
-  async getEnv(): Promise<string> {
+  async onModuleInit() {
     if (!this.packageVersion) {
       const packageJson = await fs.readFile('package.json', {
         encoding: 'utf-8',
       });
       this.packageVersion = JSON.parse(packageJson).version;
     }
+  }
+
+  @Get()
+  @AllowAnonymous()
+  async getEnv(): Promise<string> {
     return `node-version: ${process.env.NODE_ENV} - db: ${process.env.DB_HOST} - version: ${this.packageVersion}`;
   }
 
