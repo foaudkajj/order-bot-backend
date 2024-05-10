1b1f76d02f152c36aa453d894c9384c9b697e789
mfuatnuroglu <mfuatnuroglu@gmail.com>
2024-05-10 09:04:43 +0300

    Fix undefined version

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,22 +2,30 @@ import {Controller, Get} from '@nestjs/common';
 import {AppService} from './app.service';
 import {AllowAnonymous} from './panel/decorators/public.decorator';
 import {UIResponseBase} from './panel/dtos';
+import {promises as fs} from 'fs';
 
 @Controller('api/app')
 export class AppController {
+  packageVersion: string;
   constructor(private readonly appService: AppService) {}
 
   @Get()
   @AllowAnonymous()
-  getEnv(): string {
-    return `node-version: ${process.env.NODE_ENV} - db: ${process.env.DB_HOST} - version: ${require('../package.json').version}`;
+  async getEnv(): Promise<string> {
+    if (!this.packageVersion) {
+      const packageJson = await fs.readFile('package.json', {
+        encoding: 'utf-8',
+      });
+      this.packageVersion = JSON.parse(packageJson).version;
+    }
+    return `node-version: ${process.env.NODE_ENV} - db: ${process.env.DB_HOST} - version: ${this.packageVersion}`;
   }
 
   @Get('version')
   @AllowAnonymous()
   getVersion(): UIResponseBase<string> {
     return <UIResponseBase<string>>{
-      data: `${require('../package.json').version}`,
+      data: `${this.packageVersion}`,
     };
   }
 }
